2e6e990e109f777f1bf121b6f4ebe602e50e304d
Ranika Williams <rnkprice07@gmail.com>
2019-10-26 19:09:42 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,8 +22,14 @@ class PostsController < ApplicationController
 	  @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
-
+	
 	def edit
 	  @post = Post.find(params[:id])
+	end
+	
+	private
+	
+	def post_params
+	  params.require(:post).permit(:title, :description)
 	end
 end
